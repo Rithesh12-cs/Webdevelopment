7638a42dd423346a2cc946a3351eca45ba477c80
rithesh <vmrpg23@gmail.com>
2026-07-10 00:03:36 +0530

    Add practice folder and remove old files

--- test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-# From a local file
-st.image("sam.jfif", caption="Local Image")
-
-# From a URL
-st.image("https://streamlit.io/images/brand/streamlit-logo-secondary-colormark-darktext.png", width=200)
